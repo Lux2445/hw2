96ac7c5b3a0a452564d2659ff2c451f18c1c3975
unknown <yosinnurov2007@gmail.com>
2024-10-03 17:49:24 +0500

    new

---
+++ startWhithUpperCase.js
@@ -0,0 +1,16 @@
+function startWhithUpperCase(str) {
+    return /^[A-Z]/.test(str);
+}
+
+const strings = [
+    'Hello world',
+    'hello world',
+    'JavaScript is fun',
+    '123abc',
+    'Good Morning!',
+    ''
+];
+
+strings.forEach(s => {
+    console.log(`Строка: "${s}", Начинается с заглавной буквы: ${startWhithUpperCase(s)}`);
+});
